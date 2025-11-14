0e18285fb5cde92c9297c873efbe8e34f7ecc417
Sakuria1209 <Sakuria1209@outlook.com>
2025-11-14 22:42:31 +0900

    ビューページの追加

--- novel_app/lib/screens/generatepage.dart
+++ novel_app/lib/screens/generatepage.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
+import 'viewpage.dart';
 
 class GenerateNovelPage extends StatefulWidget {
   const GenerateNovelPage({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   @override
   State<GenerateNovelPage> createState() => _GenerateNovelPageState();
@@ -43,6 +35,17 @@ class _GenerateNovelPageState extends State<GenerateNovelPage> {
                 setState(() {
                   isSelectedValue = value!;
                 });
+              },
+            ),
+            ElevatedButton(
+              child: Text("ViewPage"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ViewNovelPage(title: "ここに小説のタイトル"),
+                  ),
+                );
               },
             ),
           ],
