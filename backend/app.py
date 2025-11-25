e75fe4f3778e71eb7df39c32eedcf0f22e66c46d
KikuzCom1207 <231205075@ccmailg.meijo-u.ac.jp>
2025-11-25 09:16:56 +0000

    fix: CORS設定を追記

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,22 @@
 import logging
 
 from flask import Flask
+from flask_cors import CORS
 from flask_restx import Api, Resource
 
 from src.database import init_db
-from src.seeds.genres import seed_genres
 from src.genres import api as genres_api
 from src.genres import genres_module
 from src.novels import api as novels_api
 from src.novels import novels_module
+from src.seeds.genres import seed_genres
 from src.tests import api as test_api
 from src.tests import test_module
 from src.users import api as user_api
 from src.users import users_module
 
 # Configure logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 app = Flask(__name__)
 
@@ -45,6 +43,16 @@ api.add_namespace(novels_api, path="/novels")
 # /users以下.
 app.register_blueprint(users_module)
 api.add_namespace(user_api, path="/users")
+
+# CORSの設定
+CORS(app)
+
+CORS(
+    app,
+    origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Authorization"],
+)
 
 
 @api.route("/hello")
