6f118bc053801ff3179b14d1175a336f204a3d47
Hayato Wakasugi <Wak8810@users.noreply.github.com>
2025-11-27 23:22:38 +0900

    feat: ユーザーの小説一覧をAPIから取得できるように - novel型の定義に，必要な値を追加と，使わない値は適当な値を代入するように - 変数及び表示する物の変更に応じて，一部変更

--- novel_app/lib/models/novel.dart
+++ novel_app/lib/models/novel.dart
@@ -5,20 +5,29 @@ class Novel {
   final String content;
   final DateTime createdAt;
   final int readingMinutes; // 読むのにかかる時間（分）
+  final String novelId;
+  final String overallPlot;
+  final int textLength;
 
   Novel({
     required this.title,
-    required this.content,
+    this.content = '', // content をデフォルト値付きの非nullable引数に変更
     required this.createdAt,
-    required this.readingMinutes,
+    this.readingMinutes = 0,
+    required this.novelId,
+    required this.overallPlot,
+    required this.textLength,
   });
 
   factory Novel.fromJson(Map<String, dynamic> json) {
     return Novel(
       title: json['title'] as String,
-      content: json['content'] as String,
+      content: json['content'] as String? ?? '',
+      readingMinutes: json['readingMinutes'] as int? ?? 0,
       createdAt: DateTime.parse(json['created_at'] as String),
-      readingMinutes: json['reading_minutes'] as int,
+      novelId: json['novel_id'] as String,
+      overallPlot: json['overall_plot'] as String,
+      textLength: json['text_length'] as int,
     );
   }
 }
--- novel_app/lib/utils/get_user_all_novels.dart
+++ novel_app/lib/utils/get_user_all_novels.dart
@@ -1,64 +1,42 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:novel_app/models/novel.dart';
 
 class GetUserAllNovels {
+  // static Future<List<Novel>> fetchNovels() async {
+  //   await Future.delayed(const Duration(seconds: 1));
+
+  //   final String dummyNovelsJson = """
+  //     [
+  //       {
+  //         "novel_id": "dummy_id_1",
+  //         "title": "短縮ダミー小説 1",
+  //         "overall_plot": "プロット1",
+  //         "text_length": 1000,
+  //         "created_at": "2025-11-15T10:00:00Z"
+  //       },
+  //       {
+  //         "novel_id": "dummy_id_2",
+  //         "title": "短縮ダミー小説 2",
+  //         "overall_plot": "プロット2",
+  //         "text_length": 2000,
+  //         "created_at": "2025-11-15T14:30:00Z"
+  //       }
+  //     ]
+  //   """;
+
+  //   final List<dynamic> decodedJson = json.decode(dummyNovelsJson);
+  //   return decodedJson.map((json) => Novel.fromJson(json as Map<String, dynamic>)).toList();
+  // }
+
   static Future<List<Novel>> fetchNovels() async {
-    await Future.delayed(const Duration(seconds: 1));
+    final response = await http.get(Uri.parse('http://127.0.0.1:5000/novels/'));
 
-    final String dummyNovelsJson = """
-      [
-        {
-          "title": "普通の小説",
-          "content": "普通の内容",
-          "created_at": "2025-11-15T10:00:00Z",
-          "reading_minutes": 5
-        },
-        {
-          "title": "短",
-          "content": "短",
-          "created_at": "2025-11-15T14:30:00Z",
-          "reading_minutes": 1
-        },
-        {
-          "title": "長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説",
-          "content": "長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容",
-          "created_at": "2025-11-15T08:45:00Z",
-          "reading_minutes": 15
-        },
-        {
-          "title": "長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説",
-          "content": "長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容",
-          "created_at": "2025-11-15T08:45:00Z",
-          "reading_minutes": 15
-        },
-        {
-          "title": "長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説",
-          "content": "長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容",
-          "created_at": "2025-11-15T08:45:00Z",
-          "reading_minutes": 15
-        },
-        {
-          "title": "長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説",
-          "content": "長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容",
-          "created_at": "2025-11-15T08:45:00Z",
-          "reading_minutes": 15
-        },
-        {
-          "title": "長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説",
-          "content": "長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容",
-          "created_at": "2025-11-15T08:45:00Z",
-          "reading_minutes": 15
-        },
-        {
-          "title": "長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説長い小説",
-          "content": "長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容長い内容",
-          "created_at": "2025-11-15T08:45:00Z",
-          "reading_minutes": 15
-        }
-      ]
-    """;
-
-    final List<dynamic> decodedJson = json.decode(dummyNovelsJson);
-    return decodedJson.map((json) => Novel.fromJson(json as Map<String, dynamic>)).toList();
+    if (response.statusCode == 200) {
+      final List<dynamic> decodedJson = json.decode(utf8.decode(response.bodyBytes));
+      return decodedJson.map((json) => Novel.fromJson(json as Map<String, dynamic>)).toList();
+    } else {
+      throw Exception('Failed to load novels');
+    }
   }
 }
