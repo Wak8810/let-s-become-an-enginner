a5013f4c6ae568938968bfcc01ca0d81363bc87e
Hayato Wakasugi <Wak8810@users.noreply.github.com>
2025-11-29 21:37:35 +0900

    feat: アプリの起動前にuserIdの有無を確認するように - userIdがない場合，作成してストレージに保存した後，userIdを渡す - ある場合，そのuserIdを渡す

--- novel_app/lib/main.dart
+++ novel_app/lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:novel_app/screens/novel_list/novel_list_screen.dart';
+import 'package:novel_app/utils/user_api.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  const storage = FlutterSecureStorage();
+  String? userId = await storage.read(key: 'user_id');
+  if (userId == null || userId.isEmpty) {
+    final newUser = await registerUser();
+    userId = newUser.id;
+    await storage.write(key: 'user_id', value: userId);
+  }
+  runApp(MyApp(userId: userId));
 }
 
+
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.userId});
+  final String userId;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const NovelListScreen(),
+      home: NovelListScreen(userId: userId),
     );
   }
 }
